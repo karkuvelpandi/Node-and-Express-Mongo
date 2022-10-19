a07d49788687735d1c57a65d707520ed8ea018d7
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-10-19 18:53:19 +0530

    ok

--- react-01/src/Components/Admin.jsx
+++ react-01/src/Components/Admin.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import Axios from 'axios'
 
 const Admin = () => {
+  let [products,setProducts] = useState([])
+  useEffect(()=>{
+    Axios.get("http://127.0.0.1:5000/api/products").then((res)=>{
+      setProducts(res.data)
+    }).catch(()=>{})
+  },[])
   return (
-    <div>Admin</div>
+    <>
+      <div className="container mt-5">
+        <pre>{JSON.stringify(products)}</pre>
+        <div className="row">
+        <div className="col-8">
+          <table>
+         <thead>
+
+         </thead>
+        <tbody>
+        {
+          products.length > 0 ? <>
+            {
+                products.map(()=>{
+                  return 
+                })
+            }
+          </> : <><h1>No Products are created</h1></>
+        }
+        </tbody>
+        </table>
+      </div>
+      </div>
+        </div>
+    </>
   )
 }
 
