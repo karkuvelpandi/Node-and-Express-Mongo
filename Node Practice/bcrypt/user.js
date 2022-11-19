09db93360cf9ba47aaad0aa0ae1007a4f07c71d7
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-11-19 10:18:55 +0530

    'ok'

--- Node Practice/bcrypt/user.js
+++ Node Practice/bcrypt/user.js
@@ -5,3 +5,7 @@ let user={
     password:"abc123",
     cc:"457898653212"
 }
+let salt=bcrypt.genSaltSync(10)
+let new_password=bcrypt.hashSync(user.password,salt)
+console.log(user.password);
+console.log(new_password);
