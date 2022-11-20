18bdb3cf56080b0af14890ef092933e88457c80b
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-11-20 13:06:27 +0530

    ok

---
+++ Node Practice/Third_Party_Modules/dotenv/App.js
@@ -0,0 +1,11 @@
+const dotenv=require('dotenv')
+const path=require('path')
+
+const file=path.join(__dirname,'config','config.env')
+
+
+// dotenv.config({path:'./config/config.env'})
+dotenv.config({path:file})
+
+console.log(process.env.PORT);  //8080
+console.log(process.env.SECRET_KEY);
