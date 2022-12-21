78894dff248a2db46f68c68fa140c64001c81e9c
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-12-21 20:11:28 +0530

    ok

--- rubixe-react-node/src/components/Home.jsx
+++ rubixe-react-node/src/components/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import InitiativeCard from './InitiativeCard/InitiativeCard'
+import Header from './Header/Header'
+import History from './History/History'
+import Form from './Form/Form'
+import Footer from './Footer/Footer'
 const Home = () => {
   return <>
-  /\
-  
+  <div><Header/></div>
+  <div><History/></div>
+  <div> <InitiativeCard/></div>
+  <div><Form/></div>
+  <div><Footer/></div>
   </>
 }
 
---
+++ rubixe-react-node/src/components/WhoWe/WhoWe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WhoWe = () => {
+  return <>
+  <h1>WhoWe</h1>
+  </>
+}
+
+export default WhoWe
