4b9a5a77f499637c3a56d88e408357da067b1d90
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-12-22 18:31:50 +0530

    ok

--- rubixe-react-node/src/components/InitiativeCard/InitiativeCard.jsx
+++ rubixe-react-node/src/components/InitiativeCard/InitiativeCard.jsx
@@ -6,29 +6,29 @@ const InitiativeCard = () => {
     <div><h1>TECH FOR TEENS -A RUBIXE @ INITIATIVE</h1></div>
     <div className='card-content'> 
       <div className='card-1'>
-        <div className="card-head radius-top">sjklfjsdf</div>
-        <div className="card-body">sjfjjsljdf</div>
-        <div className="card-tail">sjdfkhsdf</div>
+        <div className="card-head radius-top" style={{backgroundColor:'#1d2d60'}}></div>
+        <div className="card-body"></div>
+        <div className="card-tail"></div>
       </div>
       <div className='card-2'>
-        <div className="card-tail">sjdfkhsdf</div>
-        <div className="card-body">sjfjjsljdf</div>
-      <div className="card-head radius-bottom">sjklfjsdf</div>
+        <div className="card-tail"></div>
+        <div className="card-body"></div>
+      <div className="card-head radius-bottom card-head-down" style={{backgroundColor:'#9d2057'}}></div>
       </div>
       <div className='card-1'>
-      <div className="card-head radius-top">sjklfjsdf</div>
-        <div className="card-body">sjfjjsljdf</div>
-        <div className="card-tail">sjdfkhsdf</div>
+      <div className="card-head radius-top" style={{backgroundColor:'#e35e13'}}></div>
+        <div className="card-body"></div>
+        <div className="card-tail"></div>
       </div>
       <div className='card-2'>
-      <div className="card-tail">sjdfkhsdf</div>
-        <div className="card-body">sjfjjsljdf</div>
-      <div className="card-head radius-bottom">sjklfjsdf</div>
+      <div className="card-tail"></div>
+        <div className="card-body"></div>
+      <div className="card-head radius-bottom card-head-down" style={{backgroundColor:'#195b37'}}></div>
       </div>
       <div className='card-1'>
-      <div className="card-head radius-top">sjklfjsdf</div>
-        <div className="card-body">sjfjjsljdf</div>
-        <div className="card-tail">sjdfkhsdf</div>
+      <div className="card-head radius-top"style={{backgroundColor:'#b77d33'}}></div>
+        <div className="card-body"></div>
+        <div className="card-tail"></div>
       </div>
     </div>
   </div>
