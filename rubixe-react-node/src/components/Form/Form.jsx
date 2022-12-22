ef8c60480bfe314032c4a13c2268335ad0f5d5bf
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-12-22 21:44:52 +0530

    ok

--- rubixe-react-node/src/components/Form/Form.jsx
+++ rubixe-react-node/src/components/Form/Form.jsx
@@ -8,12 +8,13 @@ const Form = () => {
       <h3 className='h3-text'>Please complete the form and we will get back to you.</h3>
       </div>
       <div className="form-right">
-        <label>Name*</label><br />
-        <input className='input' type="text" /><br />
-        <label>Email*</label><br />
-        <input className='input' type="text" /><br />
-        <label>Mobile Number*</label><br />
-        <input className='input' type="text" />
+        <label className='label'>Name*</label><br />
+        <input className='input' type="text" placeholder='Enter Your Name' /><br />
+        <label className='label'>Email*</label><br />
+        <input className='input' type="text" placeholder='Enter Your Email' /><br />
+        <label className='label'>Mobile Number*</label><br />
+        <input className='input' type="text" placeholder='Enter Your Mobile Number' /><br /><br />
+        <button className='input btn'>Register Now</button>
       </div>
      </div>
   
