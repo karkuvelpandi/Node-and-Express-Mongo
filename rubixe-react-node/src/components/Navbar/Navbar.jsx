b11769420eb83465e509b114e75ee31da702ae79
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-12-22 21:51:48 +0530

    ok

--- rubixe-react-node/src/components/Navbar/Navbar.jsx
+++ rubixe-react-node/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link}from 'react-router-dom'
 import homeLogo from '../../assets/logo.png'
 import './Navbar.css'
 const Navbar = () => {
+  const [scrolled,setScrolled]= useState(false)
+  const scrollToActive=()=>{
+    if(window.scrollY>=75){
+      setScrolled (true);
+    } else { setScrolled(false)}
+  }
+  window.addEventListener("scroll", scrollToActive);
   return <>
-  <nav className='nav'>
+  <nav className={scrolled?"nav-fixer":"nav"}>
     <div className='nav-left'>
     <Link to='/'><img src={homeLogo} className='homeLogo' alt="" /></Link>
     </div>
