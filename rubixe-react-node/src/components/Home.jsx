54a4ba6e3d2fa3330748f894c6f414507dab7933
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-12-22 17:44:54 +0530

    ok

--- rubixe-react-node/src/components/Home.jsx
+++ rubixe-react-node/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import InitiativeCard from './InitiativeCard/InitiativeCard'
 import Header from './Header/Header'
+import WhoWe from './WhoWe/WhoWe'
 import History from './History/History'
 import Form from './Form/Form'
 import Footer from './Footer/Footer'
 const Home = () => {
   return <>
   <div><Header/></div>
+  <div><WhoWe/></div>
   <div><History/></div>
   <div> <InitiativeCard/></div>
   <div><Form/></div>
--- rubixe-react-node/src/components/InitiativeCard/InitiativeCard.jsx
+++ rubixe-react-node/src/components/InitiativeCard/InitiativeCard.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
-
+import './InitiativeCard.css'
 const InitiativeCard = () => {
-  return (
-    <div>InitiativeCard</div>
-  )
+  return<>
+  <div className="card-div">
+    <div><h1>TECH FOR TEENS -A RUBIXE @ INITIATIVE</h1></div>
+    <div className='card-content'> 
+      <div className='card-1'>
+        <div className="card-head radius-top">sjklfjsdf</div>
+        <div className="card-body">sjfjjsljdf</div>
+        <div className="card-tail">sjdfkhsdf</div>
+      </div>
+      <div className='card-2'>
+        <div className="card-tail">sjdfkhsdf</div>
+        <div className="card-body">sjfjjsljdf</div>
+      <div className="card-head radius-bottom">sjklfjsdf</div>
+      </div>
+      <div className='card-1'>
+      <div className="card-head radius-top">sjklfjsdf</div>
+        <div className="card-body">sjfjjsljdf</div>
+        <div className="card-tail">sjdfkhsdf</div>
+      </div>
+      <div className='card-2'>
+      <div className="card-tail">sjdfkhsdf</div>
+        <div className="card-body">sjfjjsljdf</div>
+      <div className="card-head radius-bottom">sjklfjsdf</div>
+      </div>
+      <div className='card-1'>
+      <div className="card-head radius-top">sjklfjsdf</div>
+        <div className="card-body">sjfjjsljdf</div>
+        <div className="card-tail">sjdfkhsdf</div>
+      </div>
+    </div>
+  </div>
+  </>
 }
 
 export default InitiativeCard
