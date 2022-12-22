6302e01b26a60d9fdd6c4ac99ff72150601aca07
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-12-22 20:15:10 +0530

    ok

--- rubixe-react-node/src/components/Form/Form.jsx
+++ rubixe-react-node/src/components/Form/Form.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import './Form.css'
 const Form = () => {
   return <>
-   
+     <div className="form-container">
+      <div className="form-left">
+      <h1>GET IN TOUCH</h1>
+      <h3 className='h3-text'>Please complete the form and we will get back to you.</h3>
+      </div>
+      <div className="form-right">
+        <label>Name*</label><br />
+        <input className='input' type="text" /><br />
+        <label>Email*</label><br />
+        <input className='input' type="text" /><br />
+        <label>Mobile Number*</label><br />
+        <input className='input' type="text" />
+      </div>
+     </div>
   
   </>
 }
