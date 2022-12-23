bde166121098c00edf08a78ec949793de566ca00
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-12-23 10:50:57 +0530

    ok

--- rubixe-node-server/router/userRouter.js
+++ rubixe-node-server/router/userRouter.js
@@ -3,8 +3,14 @@ import User from '../model/User.js'
 
 const router=express.Router()
 //get all users
-router.get('/',(req,resp)=>{
-
+/*
+URL            : http://127.12.22.32:8000/user/
+method         : GET
+required field : N/A
+*/
+router.get('/',async(req,resp)=>{
+   let user=await User.find()
+   resp.status(200).json(user)
 })
 
 //Register user  / creating user data
@@ -25,13 +31,92 @@ router.post('/register',async(req,resp)=>{
             description:req.body.description,
             image:req.body.image
         }
-        let user=await User.findOne({email:new_user})
+        let user=await User.findOne({email:new_user.email})
+        if(user){
+            resp.status(200).json({
+                msg:'User already exist...'
+            })
+        }
         let createUser=await User(new_user)
+      let saveUser=await createUser.save()
+      resp.status(200).json({
+        result:"User Register Successfully...",
+        user:saveUser
+      })
 
       }
       catch(err){}
    
 })
-      
+// Updating user data
+/*
+URL            : http://127.12.22.32:8000/user/:id
+method         : POST
+required field : name,mobile,email,password,state,city,description,image
+*/
+router.put("/:id",async(req,resp)=>{
+    let user_Id=req.params.id
+    try{
+       let updateUser={
+        name:req.body.name,
+        mobile:req.body.mobile,
+        email:req.body.email,
+        password:req.body.password,
+        state:req.body.state,
+        city:req.body.city,
+        description:req.body.description,
+        image:req.body.image
+       }
+       let user=await User.findById(user_Id)
+       if(!user){
+         return resp.status(404).json({msg:"User not found...!"})
+       }
+       user=await User.findByIdAndUpdate(user_Id,{$set:updateUser},{new:true})
+       resp.status(200).json({
+         result : 'Product updated successfully...!',
+         user:user
+       })
+    }
+    catch(err){}
+ })
+
+ // Deleting user data
+/*
+URL            : http://127.12.22.32:8000/user/:id
+method         : DELETED
+required field : N/A
+*/
+ router.delete('/:id',async(req,resp)=>{
+    let user_Id=req.params.id
+    try{
+      let user=await User.findById(user_Id)
+      if(!user){
+         return resp.status(404).json({msg:"Product not found...!"})
+      }
+      user=await User.findByIdAndDelete(user_Id)
+      resp.status(200).json({msg:'Product deleted successfully...!'})
+    }
+    catch(err){}
+ })
+
+ //Get single user
+/*
+URL            : http://127.12.22.32:8000/user/:id
+method         : GET
+required field : N/A
+*/
+ router.get('/:id',async (req,resp)=>{
+    let user_Id=req.params.id
+    try{
+     let user=await User.findById(user_Id)
+      if(!user){
+         return resp.status(404).json({
+             msg : "Product not found...!"
+         })
+      }
+      resp.status(200).json(user)
+    }
+    catch(err){}
+ })
 
 export default router
